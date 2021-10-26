22dfcbb95380f5c0d7d40e6fb69700d7e7cdff2d
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-10-27 00:00:54 +0200

    add cmake-init

---
+++ pkgs/cmake-init/default.nix
@@ -0,0 +1,25 @@
+{ lib, buildPythonPackage, fetchFromGitHub, fetchPypi, python, git, cmake, clang-tools, cppcheck, lcov, codespell }:
+buildPythonPackage rec {
+  pname = "cmake-init";
+  version = "0.22.0";
+  format = "wheel";
+
+  src = fetchPypi {
+    inherit version format;
+    pname = "cmake_init";
+    python = "py3";
+    sha256 = "sha256-tDC7PNxr5w7efvPPMgcrVYh3O/48f3RW+84CVTjEblo=";
+  };
+
+  nativeBuildInputs = [ python git cmake ];
+  buildInputs = [ cmake git clang-tools cppcheck lcov codespell ]; 
+
+  doCheck = false;
+  dontUseCmakeConfigure = true;
+
+  meta = with lib; {
+    description = "Modern CMake (3.14+) project initializer that generates FetchContent-ready projects, separates consumer and developer targets, provides install rules with proper relocatable CMake packages.";
+    license = licenses.gpl3;
+    platforms = platforms.unix;
+  };
+}
