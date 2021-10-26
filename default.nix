64ec38495c1be5c01c74cbbe65e26cbd67737d14
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-10-26 19:23:14 +0200

    add eovim

--- default.nix
+++ default.nix
@@ -15,8 +15,8 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   asmjit = pkgs.callPackage ./pkgs/asmjit { };
+  eovim = pkgs.callPackage ./pkgs/eovim { };
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
