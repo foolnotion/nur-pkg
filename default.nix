e55abd7c1097280f9b4f68e82b6d8b235ebd2cd7
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-10-26 10:46:43 +0200

    add asmjit

--- default.nix
+++ default.nix
@@ -14,6 +14,8 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
+  asmjit = pkgs.callPackage ./pkgs/asmjit { };
+
   example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
