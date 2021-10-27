c15aa97937b22b3684280ff2c8593b9715fced6e
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-10-28 00:31:22 +0200

    add keyd

---
+++ pkgs/keyd/default.nix
@@ -0,0 +1,32 @@
+{ lib, stdenv, fetchgit, udev, git }:
+
+let
+  pname = "keyd";
+  version = "1.1.2";
+in stdenv.mkDerivation rec {
+  inherit version pname;
+
+  src = fetchgit {
+    url = "https://github.com/rvaiya/keyd.git";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-E9WHCJxTGwvkrwm4zXFye53nJU9GFtovIxRrpoL/VtM=";
+  };
+
+  buildInputs = [ git udev ];
+
+  patches = [ ./keyd.patch ];
+
+  buildPhase = ''
+    make LOCK_FILE=/tmp/keyd.lock LOG_FILE=/tmp/keyd.log
+  '';
+
+  installPhase = ''
+    make install DESTDIR=$out PREFIX=
+  '';
+
+  meta = {
+    description = "A key remapping daemon for linux.";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+  };
+}
