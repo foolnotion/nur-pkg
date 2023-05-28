3705cb438f0932a607f5599e42f13d5b40d7a6e5
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-05-28 09:56:08 +0200

    [trng]: update to rev. efdb3cfdb

--- pkgs/trng/default.nix
+++ pkgs/trng/default.nix
@@ -1,17 +1,22 @@
-{ lib, stdenv, fetchFromGitHub, cmake }:
+{ lib, stdenv, cmake }:
 
 stdenv.mkDerivation rec {
   pname = "trng";
   version = "4.24";
 
-  src = fetchFromGitHub {
-    owner = "rabauke";
-    repo = "trng4";
-    rev = "v${version}";
-    sha256 = "sha256-yqiUe47ydOWB5QFaHjGEh1QJph+2LKqV75fM9BQwtxA=";
+  src = builtins.fetchGit {
+    url = "https://github.com/rabauke/trng4.git";
+    rev = "efdb3cfdb589a1f34ce4dfde85a8a068451018e3";
+    submodules = true;
   };
 
   nativeBuildInputs = [ cmake ];
+
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TESTING=0"
+    "-DBUILD_SHARED_LIBS=0"
+  ];
+
 
   meta = with lib; {
     description = "Modern C++ pseudo random number generator library";
