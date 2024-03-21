e50d45cc7c59759d4b1c55f524984151d0beb332
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-03-21 15:04:28 +0100

    cpptrace: init at 0.5.1

--- pkgs/cpptrace/default.nix
+++ pkgs/cpptrace/default.nix
@@ -1,27 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cmake }:
-let
-  # use a fork of libdwarf with proper cmake support (needed by cpptrace)
-  libdwarf_ = stdenv.mkDerivation {
-    pname = "libdwarf";
-    version = "0.9.1";
-
-    src = fetchFromGitHub {
-      owner = "jeremy-rifkin";
-      repo = "libdwarf-code";
-      rev = "520e20185262f96c9580cab157307b0890fc2c66";
-      hash = "sha256-AlrdgUFPC9wQp851yHdwmCD/6dRvH/EphKCercO4wbs=";
-    };
-
-    nativeBuildInputs = [ cmake ];
-
-    meta = {
-      homepage = "https://github.com/jeremy-rifkin/libdwarf-code";
-      platforms = lib.platforms.unix;
-      license = lib.licenses.lgpl21Plus;
-      maintainers = [ lib.maintainers.atry ];
-    };
-  };
-in
+{ lib, stdenv, fetchFromGitHub, cmake, zstd, libdwarf }:
 stdenv.mkDerivation rec {
   pname = "cpptrace";
   version = "0.5.1";
@@ -35,11 +12,12 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake ];
 
-  buildInputs = [ libdwarf_ ];
+  buildInputs = [ libdwarf zstd ];
 
   cmakeFlags = [
     "-DCPPTRACE_GET_SYMBOLS_WITH_LIBDWARF=1"
     "-DCPPTRACE_USE_EXTERNAL_LIBDWARF=1"
+    "-DCPPTRACE_USE_EXTERNAL_ZSTD=1"
   ];
 
   meta = with lib; {
@@ -47,6 +25,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/jeremy-rifkin/cpptrace";
     license = licenses.mit;
     platforms = platforms.all;
-    #maintainers = with maintainers; [ foolnotion ];
+    maintainers = with lib.maintainers; [ foolnotion ];
   };
 }
